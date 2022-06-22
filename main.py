66eeede0e760addfcdb23083f0ffefd12230674e
Artur Zacniewski <a.zacniewski@gmail.com>
2022-06-22 16:52:24 +0200

    Pydantic in action + checking different HTTP methods

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
 
 app = FastAPI()
 
@@ -6,3 +17,32 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello FastAPI"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {'item_id': item_id}
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+@app.post("/items")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
